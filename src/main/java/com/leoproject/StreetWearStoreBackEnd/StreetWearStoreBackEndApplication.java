45826c340af987b43f4dd1339afa858078bf2d31
LeonamPrado <leonamopprado@gmail.com>
2024-05-17 14:30:06 -0300

    project created

---
+++ src/main/java/com/leoproject/StreetWearStoreBackEnd/StreetWearStoreBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.leoproject.StreetWearStoreBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetWearStoreBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetWearStoreBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leoproject/StreetWearStoreBackEnd/StreetWearStoreBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoproject.StreetWearStoreBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetWearStoreBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
